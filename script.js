2023ef2749b98d05b8732dfef2b8d4ad0c0e0066
Francisamos123 <sugartone45@yahoo.com>
2023-05-05 16:25:07 +0000

    commit to main

--- script.js
+++ script.js
@@ -19,10 +19,48 @@ const items = [
         name:"flask",image:"flask.png"
     },
     {
-        name:"1",image:"anchor.png"
+        name:"bug",image:"bug.png"
     },
     {
-        name:"1",image:"anchor.png"
+        name:"car",image:"car.png"
+    },
+    {
+        name:"hand-spock",image:"hand-spock.png"
+    },
+    {
+        name:"moon",image:"moon.png"
+    },
+    {
+        name:"futbol",image:"futbol.png"
+    },
+    {
+        name:"lira-sign",image:"lira-sign.png"
+    },
+    {
+        name:"snowflake",image:"snokeflake.png"
     },
 
 ];
+
+
+// ----INITIAL TIME-----
+let seconds  = 0;
+let minutes = 0;
+let movesCount = 0;
+
+//----FOR TIMER -----
+const timeGenerator = () => {
+    seconds +=1;
+}
+
+// --- LOGIC FOR MINUTES
+ if(seconds >= 60){
+    minutes += 1;
+    seconds = 0;
+ }
+
+// ---- FORMAT TIME BEFORE DISPLAYING-----
+
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+timeValue.innerHTML =  ``
