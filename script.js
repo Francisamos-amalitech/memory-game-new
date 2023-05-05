65412db2820194c0715e90b3e4e56f3b97916b47
Francisamos123 <sugartone45@yahoo.com>
2023-05-05 15:54:01 +0000

    commit to main

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let moves = document.getElementById('moves');
-let timeValue = document.getElementById('time');
-
+const moves = document.getElementById('moves');
+const timeValue = document.getElementById('time');
+const cardContainer = document.querySelector('.cardContainer');
