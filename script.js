3fdce39440d1d9f360aab82a49802d62fcf6f4fe
Francisamos123 <sugartone45@yahoo.com>
2023-05-05 16:07:18 +0000

    commit to main

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// SELECTORS
 const moves = document.getElementById('moves');
 const timeValue = document.getElementById('time');
 const cardContainer = document.querySelector('.cardContainer');
+
+//VARIABLES
+let cards;
+let interval;
+let cardFront = false;
+let cardBack = false;
+
+// ---- ITEMS IN ARRAY------
+const items = [
+    {
+        name:"anchor",image:"anchor.png"
+    },
+    
+    {
+        name:"flask",image:"flask.png"
+    },
+    {
+        name:"1",image:"anchor.png"
+    },
+    {
+        name:"1",image:"anchor.png"
+    },
+
+];
